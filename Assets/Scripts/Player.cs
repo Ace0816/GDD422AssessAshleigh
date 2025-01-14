a7cdc850b81d3ed99325e9e5ded6a748339e758e
UOC\2407008 <2407008@stu.chi.ac.uk>
2025-01-14 10:18:40 +0000

    Changed walls and pellets to Tilemap. Player movement script added with racing game player input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float yMoveSpeed = 5f;
+    [SerializeField] float zMoveSpeed = 5f;
+
+    SpriteRenderer sR;
+
+    void Start()
+    {
+        sR = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float yMoveAmount = Input.GetAxis("Vertical") * yMoveSpeed * Time.deltaTime;
+        float zMoveAmount = Input.GetAxis("Horizontal") * zMoveSpeed * Time.deltaTime;
+
+        transform.Translate(0, yMoveAmount, zMoveAmount);
+    }
+}
